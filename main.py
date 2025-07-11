1813ba2166bd0af26311d8c5aa055055bd9dcecc
JoaoMoncorvo <joaogabriel@joaogabriel.com.br>
2025-07-10 22:34:59 -0300

    otimizando programa

--- main.py
+++ main.py
@@ -1,27 +1,26 @@
-from modulos import (Cadastrar, Menu, AlterarDados, DeletarPet, ListarPets, ListarPorCriterio)        
+from modulos import (Cadastrar, Menu, AlterarDados, DeletarPet, ListarPets, ListarPorCriterio)  
+from time import sleep      
 print('BEM VINDO AO SISTEMA DO PETSHOP MONCORVO')
 print('-='*10)
 while True:
     menu = Menu()
-    if menu== 1:
-        Cadastrar()
-        continue
-    elif menu == 2:
-        AlterarDados()w
-        continue
-    elif menu == 3:
-        DeletarPet()
-        continue
-    elif menu == 4:
-        ListarPets()
-        continue
-    elif menu == 5:
-        ListarPorCriterio()
-        continue
-    elif menu == 6:
-        print('OBRIGADO POR USAR NOSSO SISTEMAS, VOLTE SEMPRE!')
-        sleep(0.)
-        break
+    match menu:
+        case 1:
+            Cadastrar()
+            continue
+        case 2:
+            AlterarDados()
+            continue
+        case 3:
+            DeletarPet()
+        case 4:
+            ListarPets()
+        case 5:
+            ListarPorCriterio()
+        case 6:
+            print('OBRIGADO POR USAR NOSSO SISTEMAS, VOLTE SEMPRE!')
+            sleep(0.5)
+            break
 
 
 
